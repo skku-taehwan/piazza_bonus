f7f2afb66cf545d5e7561ba99c5669137b742cda
Seonghee Jin <seonghee@tsnlab.com>
2022-01-04 13:19:40 +0900

    Refactor: Refactoring parsing
    change dic to defaultdic.
    defaultdic makes key automatically when key is not exists.
    refactoring parsing question, note, answer, discussion
    in parsingGoods, and parsingGoodDiscussion functions.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ###
 
+from collections import defaultdict as dic
 import random
 import sys
 import time
@@ -13,12 +14,24 @@ from selenium.webdriver.common.by import By
 ###
 
 
-def appendNameToDic(studentName: str, i: int, df: dict) -> dict:
-    if studentName in df.keys():
-        df[studentName].append(i)
-    else:
-        df[studentName] = [i]
-    return df
+def parsingGoods(soup: bs, messageParse: str, studentParse: str) -> str:
+    endorseMessage = soup.select(messageParse)
+    if endorseMessage:
+        endorseStudent = soup.select(studentParse)
+        studentName = endorseStudent[0].contents[0].text.strip()
+        return studentName
+
+
+def parsingGoodDiscuss(soup: bs, disccussParse: str, discuss: str, studentParse: str) -> str:
+    clarifyingDiscussions = soup.select(disccussParse)
+    if clarifyingDiscussions:
+        for endorseDiscussion in clarifyingDiscussions:
+            discussionAnswer = \
+                endorseDiscussion.select(discuss)
+            if discussionAnswer:
+                studentAnswer = endorseDiscussion.select_one(studentParse)
+                studentName = studentAnswer.text.strip()
+                return studentName
 
 
 def main():
@@ -28,15 +41,16 @@ def main():
         print("RUN python3 main.py [piazza_class_URL] [start_page_number] \
             [end_page_number] [piazza_ID] [piazza_PASSWD]", file=sys.stderr)
         return
+
     # Config
-    df = dict()
+    df = dic(list)
     ser = Service("./chromedriver")
     option = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=ser, options=option)
 
     # Login
     url, start, end, id, passwd = sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), sys.argv[4], sys.argv[5]
-    driver.get("{url}?cid={number}".format(url=url, number=1))
+    driver.get("{url}?cid={number}".format(url=url, number=start))
     driver.find_element(By.ID, "email_field").send_keys(id)
     driver.find_element(By.ID, "password_field").send_keys(passwd)
     driver.find_element(By.ID, "modal_login_button").click()
@@ -54,63 +68,42 @@ def main():
             soup = bs(html, 'html.parser')
 
             # parsing good Question
-            endorseQuestion = soup.select("div#question > div#view_quesiton_note > \
-            div.post_region_message_wrapper > div#endorse_text > \
-            div > span.endorse_message")
-            if endorseQuestion:
-                endorseStudent = soup.select("div#question > \
-                div#view_question_note_bar > \
-                div.post_region_actions_meta > div")
-                studentName = endorseStudent[0].contents[0].text.strip()
-                df = appendNameToDic(studentName, i, df)
-            else:
-                print("no good question")
+            question = "div#question > div#view_quesiton_note > div.post_region_message_wrapper > \
+            div#endorse_text > div > span.endorse_message"
+            student = "div#question > div#view_question_note_bar > div.post_region_actions_meta > div"
+            goodQuestion = parsingGoods(soup, question, student)
+            if goodQuestion:
+                df[goodQuestion].append(i)
+
             # parsing good Note
-            endorseNote = soup.select("div#note > div#view_quesiton_note > \
-            div.post_region_message_wrapper > div#endorse_text > \
-            div > span.endorse_message")
-            if endorseNote:
-                endorseStudent = soup.select("div#note > div#view_question_note_bar > \
-                div.post_region_actions_meta > div")
-                studentName = endorseStudent[0].contents[0].text.strip()
-                df = appendNameToDic(studentName, i, df)
-            else:
-                print("no good Note")
+            note = "div#note > div#view_quesiton_note > div.post_region_message_wrapper > div#endorse_text > \
+            div > span.endorse_message"
+            student = "div#note > div#view_question_note_bar > div.post_region_actions_meta > div"
+            goodNote = parsingGoods(soup, note, student)
+            if goodNote:
+                df[goodNote].append(i)
 
             # parsing good Answer
-            endorseAnswer = soup.select("div#member_answer > div > \
-            div.post_region_content.view_mode > \
-                div.post_region_message_wrapper > \
-                    div > div > span.endorse_message")
-            if endorseAnswer:
-                studentAnswer = soup.select("div#member_answer > div > \
-                div.post_region_actions.view_mode > div > div")
-                studentName = studentAnswer[0].contents[0].text.strip()
-                df = appendNameToDic(studentName, i, df)
-            else:
-                print("no good answer")
+            Answer = "div#member_answer > div > div.post_region_content.view_mode > \
+                div.post_region_message_wrapper > div > div > span.endorse_message"
+            student = "div#member_answer > div > div.post_region_actions.view_mode > div > div"
+            goodAnswer = parsingGoods(soup, Answer, student)
+            if goodAnswer:
+                df[goodAnswer].append(i)
             driver.implicitly_wait(1000)
 
             # parsing good Discussion
-            clarifyingDiscussions = soup.select("div#clarifying_discussion > \
-            div[data-pats=followups] > div[data-pats=followup]")
-            if clarifyingDiscussions:
-                for endorseDiscussion in clarifyingDiscussions:
-                    discussionAnswer = \
-                        endorseDiscussion.select("\
-                            div.post_region_message.endorse.show")
-                    if discussionAnswer:
-                        studentAnswer = endorseDiscussion.select_one("\
-                            div[data-pats=original_post] > div > a > div")
-                        studentName = studentAnswer.text.strip()
-                        df = appendNameToDic(studentName, i, df)
-                    else:
-                        print("no good discussion")
-            else:
-                print("no discussion")
+            discussions = "div#clarifying_discussion > div[data-pats=followups] > div[data-pats=followup]"
+            discussion = "div.post_region_message.endorse.show"
+            student = "div[data-pats=original_post] > div > a > div"
+            goodDiscussion = parsingGoodDiscuss(soup, discussions, discussion, student)
+            if goodDiscussion:
+                df[goodDiscussion].append(i)
+
         except Exception:
             continue
         print(df)
+
     # data to CSV file
     df = {x: df[x] for x in sorted(df.keys())}
     data = pd.DataFrame(list(df.items()), columns=["StudentName", "BonusId"])
